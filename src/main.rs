6380525ec650f8f0e12b32a957527a9f19fe63ae
Deepak Jacob <deepak.jacob@gmail.com>
2024-04-22 18:38:21 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::path::Path;
+use std::sync::mpsc::channel;
+
+use notify::Event;
+use notify::Result;
+use notify::Watcher;
+
+fn main() -> Result<()> {
+    let (tx, rx) = channel();
+
+    let mut watcher = notify::recommended_watcher(move |res: Result<Event>| match res {
+        Ok(event) => {
+            println!("new event {:?}", event);
+            if let Err(e) = tx.send(event.kind) {
+                println!("error sending event for {:?}", e);
+            }
+        }
+        Err(e) => println!("err       {:?}", e),
+    })?;
+
+    watcher.watch(Path::new("."), notify::RecursiveMode::Recursive)?;
+
+    loop {
+        match rx.recv() {
+            Ok(event_kind) => println!("Received: {:?}", event_kind),
+            Err(e) => {
+                println!("Receive error: {:?}", e);
+                break;
+            }
+        }
+    }
+    Ok(())
+}
